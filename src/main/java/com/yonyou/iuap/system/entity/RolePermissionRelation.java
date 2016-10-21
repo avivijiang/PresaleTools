5424cd9cf5cd140986725476bad5e6aa3ea82414
刘骏 <liujun5@yonyou.com>
2016-10-21 17:02:02 +0800

    Merge branch '3-design-data-table' into 'dev'
    add mapper
    
    Closes #3
    
    See merge request !8

---
+++ src/main/java/com/yonyou/iuap/system/entity/RolePermissionRelation.java
@@ -0,0 +1,33 @@
+package com.yonyou.iuap.system.entity;
+
+public class RolePermissionRelation {
+    private Long rolePermissionId;
+
+    private Long roleId;
+
+    private Long permissionId;
+
+    public Long getRolePermissionId() {
+        return rolePermissionId;
+    }
+
+    public void setRolePermissionId(Long rolePermissionId) {
+        this.rolePermissionId = rolePermissionId;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public Long getPermissionId() {
+        return permissionId;
+    }
+
+    public void setPermissionId(Long permissionId) {
+        this.permissionId = permissionId;
+    }
+}
---
+++ src/main/java/com/yonyou/iuap/system/entity/SystemRole.java
@@ -0,0 +1,65 @@
+package com.yonyou.iuap.system.entity;
+
+import java.util.Date;
+
+public class SystemRole {
+    private Long roleId;
+
+    private String roleName;
+
+    private String roleCode;
+
+    private String roleStatus;
+
+    private Date createTime;
+
+    private String description;
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName == null ? null : roleName.trim();
+    }
+
+    public String getRoleCode() {
+        return roleCode;
+    }
+
+    public void setRoleCode(String roleCode) {
+        this.roleCode = roleCode == null ? null : roleCode.trim();
+    }
+
+    public String getRoleStatus() {
+        return roleStatus;
+    }
+
+    public void setRoleStatus(String roleStatus) {
+        this.roleStatus = roleStatus == null ? null : roleStatus.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description == null ? null : description.trim();
+    }
+}
