dc61d9e86b5bee7dbeabee8fef03d861907e7802
hexr5 <hexr5@yonyou.com>
2016-10-25 20:55:09 +0800

    add roles

--- src/main/webapp/src/components/role-management/rolelist/rolelist.js
+++ src/main/webapp/src/components/role-management/rolelist/rolelist.js
@@ -5,7 +5,68 @@ define(['knockout', 'text!./rolelist.html', 'jquery'], function (ko, templateMar
     function RoleListViewModel(params) {
         var self = this;
         self.roleItemList = ko.observableArray([]);
+        self.isAdd = ko.observable(false);
+        self.addRoleName = ko.observable();
+        self.roleDescription = ko.observable();
 
+
+        //新增角色
+        self.addRole = function () {
+            self.isAdd(true);
+        }
+
+        //取消新增角色
+        self.cancelAddRole = function () {
+            self.addRoleName("");
+            self.roleDescription("");
+            self.isAdd(false);
+        }
+        //确认新增角色
+        self.makeSureRole = function () {
+            var url = globle_var.ctx + '/sysRole/createRole';
+            var prepostdata = {
+                "roleName": this.addRoleName(),
+                "description": this.roleDescription(),
+            };
+            var postdata = JSON.stringify(prepostdata);
+            $.ajax({
+                type: 'POST',
+                url: url,
+                data: postdata,
+                contentType: 'application/json',
+                dataType: 'json',
+                success: function (data) {
+                    if (data.code == '200') {
+                        self.addRoleName("");
+                        self.roleDescription("");
+                        self.roleItemList.push(new RoleItemViewModel(prepostdata));
+                        alert("新增成功");
+                    }
+
+
+                    self.isAdd(false);
+                }
+            });
+        }
+        //删除角色
+        self.deleteRole = function (pdata) {
+            var url = globle_var.ctx + '/sysRole/deleteRole';
+            var prepostdata = {
+                "roleId": pdata.roleId(),
+            };
+            var postdata = JSON.stringify(prepostdata);
+            $.ajax({
+                type: 'POST',
+                url: url,
+                data: postdata,
+                contentType: 'application/json',
+                dataType: 'json',
+                success: function () {
+                    self.roleItemList.remove(pdata);
+                    alert("删除成功");
+                }
+            });
+        }
         loadRoleData();
         function loadRoleData() {
             var url = globle_var.ctx + '/sysRole/queryRole';
@@ -14,18 +75,21 @@ define(['knockout', 'text!./rolelist.html', 'jquery'], function (ko, templateMar
                 url: url,
                 dataType: 'json',
                 success: function (data) {
-                    $.each(data.content, function (index, value) {
+                    $.each(data.data, function (index, value) {
                         self.roleItemList.push(new RoleItemViewModel(value));
                     });
                 }
             });
         }
     }
-    function RoleItemViewModel() {
+    function RoleItemViewModel(data) {
         var self = this;
-        self.rolename = ko.observable();
-        self.description = ko.observable();
+        self.roleId = ko.observable(data.roleId);
+        self.roleName = ko.observable(data.roleName);
+        self.isFocus = ko.observable(true);
     }
+
+
 
     // This runs when the component is torn down. Put here any logic necessary to clean up,
     // for example cancelling setTimeouts or disposing Knockout subscriptions/computeds.
