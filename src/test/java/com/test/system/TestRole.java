7beecf44b44ac392c3c03d844a62e5dd573ecbb8
桑伟 <sangwei@yonyou.com>
2016-10-25 10:29:09 +0800

    测试

---
+++ src/test/java/com/test/system/TestRole.java
@@ -0,0 +1,28 @@
+package com.test.system;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.junit.Test;
+
+import com.yonyou.iuap.utils.HttpUtil;
+
+public class TestRole {
+
+	@Test
+	public void testQueryRoles(){
+		HttpUtil http = HttpUtil.getInstance() ;
+		String url ="http://localhost:8080/Pre_project_manage/sysRole/queryRole" ;
+		try {
+			http.doGet(url) ;
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
